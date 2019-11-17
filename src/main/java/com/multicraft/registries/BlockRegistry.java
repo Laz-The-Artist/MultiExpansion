f03c7c4b44fa654eebe874e64db558e1bb976eba
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2019-11-17 22:39:48 +0000

    Add files via upload

--- src/main/java/com/multicraft/registries/BlockRegistry.java
+++ src/main/java/com/multicraft/registries/BlockRegistry.java
@@ -7,25 +7,63 @@ import com.multicraft.block.PottedBerryBushBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class BlockRegistry {
-	
+public class BlockRegistry
+{
 	public static Block BLUE_BERRY_BUSH;
 	
 	public static Block POTTED_BERRY_BUSH;
 	
+	public static Block MOSSY_BRICKS;
+	public static Block TERRACOTTA_BRICKS;
+	public static Block WHITE_TERRACOTTA_BRICKS;
+	public static Block ORANGE_TERRACOTTA_BRICKS;
+	public static Block MAGENTA_TERRACOTTA_BRICKS;
+	public static Block LIGHT_BLUE_TERRACOTTA_BRICKS;
+	public static Block YELLOW_TERRACOTTA_BRICKS;
+	public static Block LIME_TERRACOTTA_BRICKS;
+	public static Block PINK_TERRACOTTA_BRICKS;
+	public static Block GRAY_TERRACOTTA_BRICKS;
+	public static Block LIGHT_GRAY_TERRACOTTA_BRICKS;
+	public static Block CYAN_TERRACOTTA_BRICKS;
+	public static Block PURPLE_TERRACOTTA_BRICKS;
+	public static Block BLUE_TERRACOTTA_BRICKS;
+	public static Block BROWN_TERRACOTTA_BRICKS;
+	public static Block GREEN_TERRACOTTA_BRICKS;
+	public static Block RED_TERRACOTTA_BRICKS;
+	public static Block BLACK_TERRACOTTA_BRICKS;
+	
 	@SubscribeEvent
-	public static void registerBlocks(final RegistryEvent.Register<Block> blockRegistryEvent) {
-		
-		blockRegistryEvent.getRegistry().registerAll(
+	public static void registerBlocks(final RegistryEvent.Register<Block> blockRegistryEvent)
+	{
+		blockRegistryEvent.getRegistry().registerAll
+		(
+				//Regular
+				MOSSY_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.RED).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_mossy")),
+				TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.ADOBE).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta")),
+				WHITE_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.WHITE_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_white")),
+				ORANGE_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.ORANGE_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_orange")),
+				MAGENTA_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.MAGENTA_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_magenta")),
+				LIGHT_BLUE_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_light_blue")),
+				YELLOW_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.YELLOW_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_yellow")),
+				LIME_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.LIME_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_lime")),
+				PINK_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.PINK_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_pink")),
+				GRAY_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.GRAY_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_gray")),
+				LIGHT_GRAY_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_GRAY_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_light_gray")),
+				CYAN_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.CYAN_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_cyan")),
+				PURPLE_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.PURPLE_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_purple")),
+				BLUE_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BLUE_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_blue")),
+				BROWN_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BROWN_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_brown")),
+				GREEN_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.GREEN_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_green")),
+				RED_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.RED_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_red")),
+				BLACK_TERRACOTTA_BRICKS = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BLACK_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_black")),
 				
+				//Special
 				BLUE_BERRY_BUSH = new BlueBerryBushBlock(Block.Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().sound(SoundType.SWEET_BERRY_BUSH)).setRegistryName(Multicraft.multicraftLocation("block_blue_berry_bush")),
 				POTTED_BERRY_BUSH = new PottedBerryBushBlock(Block.Properties.create(Material.MISCELLANEOUS).tickRandomly().hardnessAndResistance(0)).setRegistryName(Multicraft.multicraftLocation("block_potted_berry_harvested"))
-				
 		);
-		
 	}
-	
 }
--- src/main/java/com/multicraft/registries/ItemRegistry.java
+++ src/main/java/com/multicraft/registries/ItemRegistry.java
@@ -2,14 +2,18 @@ package com.multicraft.registries;
 
 import com.multicraft.Multicraft;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.BlockNamedItem;
 import net.minecraft.item.Foods;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class ItemRegistry {
-	
+public class ItemRegistry
+{
 	public static Item CREATIVE_TAB_ITEM;
 	
 	public static Item BLUE_BERRIES;
@@ -21,11 +25,30 @@ public class ItemRegistry {
 	public static Item ACACIA_BARK;
 	public static Item DARK_OAK_BARK;
 	
+	public static Item MOSSY_BRICKS;
+	public static Item TERRACOTTA_BRICKS;
+	public static Item WHITE_TERRACOTTA_BRICKS;
+	public static Item ORANGE_TERRACOTTA_BRICKS;
+	public static Item MAGENTA_TERRACOTTA_BRICKS;
+	public static Item LIGHT_BLUE_TERRACOTTA_BRICKS;
+	public static Item YELLOW_TERRACOTTA_BRICKS;
+	public static Item LIME_TERRACOTTA_BRICKS;
+	public static Item PINK_TERRACOTTA_BRICKS;
+	public static Item GRAY_TERRACOTTA_BRICKS;
+	public static Item LIGHT_GRAY_TERRACOTTA_BRICKS;
+	public static Item CYAN_TERRACOTTA_BRICKS;
+	public static Item PURPLE_TERRACOTTA_BRICKS;
+	public static Item BLUE_TERRACOTTA_BRICKS;
+	public static Item BROWN_TERRACOTTA_BRICKS;
+	public static Item GREEN_TERRACOTTA_BRICKS;
+	public static Item RED_TERRACOTTA_BRICKS;
+	public static Item BLACK_TERRACOTTA_BRICKS;
+	
 	@SubscribeEvent
-	public static void registerItems(final RegistryEvent.Register<Item> itemRegistryEvent) {
-		
-		itemRegistryEvent.getRegistry().registerAll(
-				
+	public static void registerItems(final RegistryEvent.Register<Item> itemRegistryEvent)
+	{
+		itemRegistryEvent.getRegistry().registerAll
+		(
 				//BLUEBERRY_BUSH = new BlockItem(BlockRegistry.BLUEBERRY_BUSH, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(BlockRegistry.BLUEBERRY_BUSH.getRegistryName()),
 				CREATIVE_TAB_ITEM = new Item((new Item.Properties())).setRegistryName(Multicraft.multicraftLocation("creative_tab_icon")),
 				BLUE_BERRIES = new BlockNamedItem(BlockRegistry.BLUE_BERRY_BUSH, (new Item.Properties()).group(Multicraft.MULTICRAFT).food(Foods.SWEET_BERRIES)).setRegistryName(Multicraft.multicraftLocation("item_blue_berries")),
@@ -34,10 +57,27 @@ public class ItemRegistry {
 				BIRCH_BARK = new Item((new Item.Properties()).group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("item_bark_birch")),
 				JUNGLE_BARK = new Item((new Item.Properties()).group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("item_bark_jungle")),
 				ACACIA_BARK = new Item((new Item.Properties()).group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("item_bark_acacia")),
-				DARK_OAK_BARK = new Item((new Item.Properties()).group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("item_bark_dark_oak"))
+				DARK_OAK_BARK = new Item((new Item.Properties()).group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("item_bark_dark_oak")),
 				
+				//Blocks
+				MOSSY_BRICKS = new BlockItem(BlockRegistry.MOSSY_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_mossy")),
+				TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta")),
+				WHITE_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.WHITE_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_white")),
+				ORANGE_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.ORANGE_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_orange")),
+				MAGENTA_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.MAGENTA_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_magenta")),
+				LIGHT_BLUE_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.LIGHT_BLUE_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_light_blue")),
+				YELLOW_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.YELLOW_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_yellow")),
+				LIME_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.LIME_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_lime")),
+				PINK_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.PINK_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_pink")),
+				GRAY_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.GRAY_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_gray")),
+				LIGHT_GRAY_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.LIGHT_GRAY_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_light_gray")),
+				CYAN_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.CYAN_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_cyan")),
+				PURPLE_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.PURPLE_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_purple")),
+				BLUE_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.BLUE_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_blue")),
+				BROWN_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.BROWN_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_brown")),
+				GREEN_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.GREEN_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_green")),
+				RED_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.RED_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_red")),
+				BLACK_TERRACOTTA_BRICKS = new BlockItem(BlockRegistry.BLACK_TERRACOTTA_BRICKS, new Item.Properties().group(Multicraft.MULTICRAFT)).setRegistryName(Multicraft.multicraftLocation("block_brick_terracotta_black"))
 		);
-		
 	}
-	
 }
