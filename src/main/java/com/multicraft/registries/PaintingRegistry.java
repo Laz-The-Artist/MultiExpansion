26b0b0d8bc1ed5d48bafe276834a81e6ff3d62aa
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2020-03-22 23:54:25 +0000

    Reorganize PaintingRegistry
    Created a method to make stuff easier

--- src/main/java/com/multicraft/registries/PaintingRegistry.java
+++ src/main/java/com/multicraft/registries/PaintingRegistry.java
@@ -6,8 +6,8 @@ import net.minecraft.entity.item.PaintingType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class PaintingRegistry {
-	
+public class PaintingRegistry
+{
 	public static PaintingType ABSTRACT_1;
 	public static PaintingType ABSTRACT_2;
 	public static PaintingType CUBE;
@@ -29,33 +29,34 @@ public class PaintingRegistry {
 	public static PaintingType VILLAGER;
 	
 	@SubscribeEvent
-	public static void registerPaintings(final RegistryEvent.Register<PaintingType> paintingRegistryEvent) {
-		
-		paintingRegistryEvent.getRegistry().registerAll(
-				
-				
-				ABSTRACT_1 = new PaintingType(32, 64).setRegistryName(Multicraft.multicraftLocation("painting_abstract_1_by_gama")),
-				ABSTRACT_2 = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_abstract_2_by_gama")),
-				CUBE = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_cube_by_koc")),
-				END = new PaintingType(128, 80).setRegistryName(Multicraft.multicraftLocation("painting_end_by_stabo_vexo")),
-				LOCK = new PaintingType(64, 32).setRegistryName(Multicraft.multicraftLocation("painting_lock_by_gama")),
-				MARIO = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_mario_by_gama")),
-				MATRIX = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_matrix_by_gama")),
-				METEOR = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_meteor_by_gama")),
-				NETHER = new PaintingType(64, 64).setRegistryName(Multicraft.multicraftLocation("painting_nether_by_stabo_vexo")),
-				ROCK = new PaintingType(16, 16).setRegistryName(Multicraft.multicraftLocation("painting_rock_by_gama")),
-				SEA = new PaintingType(32, 16).setRegistryName(Multicraft.multicraftLocation("painting_sea_by_rellit")),
-				SUNSET = new PaintingType(64, 32).setRegistryName(Multicraft.multicraftLocation("painting_sunset_by_gama")),
-				SWORD = new PaintingType(16, 32).setRegistryName(Multicraft.multicraftLocation("painting_sword_by_gama")),
-				TRAIN = new PaintingType(128, 64).setRegistryName(Multicraft.multicraftLocation("painting_train_by_koc")),
-				TREE_1 = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_tree_1_by_gama")),
-				TREE_2 = new PaintingType(16, 16).setRegistryName(Multicraft.multicraftLocation("painting_tree_2_by_gama")),
-				TREE_3 = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_tree_3_by_gama")),
-				UNDERWORLD = new PaintingType(32, 32).setRegistryName(Multicraft.multicraftLocation("painting_underworld_by_gama")),
-				VILLAGER = new PaintingType(32, 64).setRegistryName(Multicraft.multicraftLocation("painting_villager_by_koc"))
-				
+	public static void registerPaintings(final RegistryEvent.Register<PaintingType> paintingRegistryEvent)
+	{
+		paintingRegistryEvent.getRegistry().registerAll
+		(
+				ABSTRACT_1 = register(32, 64, "painting_abstract_1_by_gama"),
+				ABSTRACT_2 = register(32, 32, "painting_abstract_2_by_gama"),
+				CUBE = register(32, 32, "painting_cube_by_koc"),
+				END = register(128, 80, "painting_end_by_stabo_vexo"),
+				LOCK = register(64, 32, "painting_lock_by_gama"),
+				MARIO = register(32, 32, "painting_mario_by_gama"),
+				MATRIX = register(32, 32, "painting_matrix_by_gama"),
+				METEOR = register(32, 32, "painting_meteor_by_gama"),
+				NETHER = register(64, 64, "painting_nether_by_stabo_vexo"),
+				ROCK = register(16, 16, "painting_rock_by_gama"),
+				SEA = register(32, 16, "painting_sea_by_rellit"),
+				SUNSET = register(64, 32, "painting_sunset_by_gama"),
+				SWORD = register(16, 32, "painting_sword_by_gama"),
+				TRAIN = register(128, 64, "painting_train_by_koc"),
+				TREE_1 = register(32, 32, "painting_tree_1_by_gama"),
+				TREE_2 = register(16, 16, "painting_tree_2_by_gama"),
+				TREE_3 = register(32, 32, "painting_tree_3_by_gama"),
+				UNDERWORLD = register(32, 32, "painting_underworld_by_gama"),
+				VILLAGER = register(32, 64, "painting_villager_by_koc")	
 		);
-		
 	}
 	
+	private static PaintingType register(int width, int height, String name)
+	{
+		return new PaintingType(width, height).setRegistryName(Multicraft.multicraftLocation(name));
+	}
 }
