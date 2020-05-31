ec8a92a31d617ad86cefc701e9b92758e8a58a91
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2020-05-31 23:00:28 +0100

    Polish berry type property

--- src/main/java/com/multicraft/block/properties/MulticraftBlockStateProperties.java
+++ src/main/java/com/multicraft/block/properties/MulticraftBlockStateProperties.java
@@ -1,11 +1,11 @@
 package com.multicraft.block.properties;
 
 import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.EnumProperty;
 
-public class MulticraftBlockStateProperties {
-	
+public class MulticraftBlockStateProperties
+{
 	public static final BooleanProperty IS_HARVESTED = BooleanProperty.create("is_harvested");
 	
-	public static final BerryTypeProperty BERRY_TYPE = BerryTypeProperty.create("berry_type", BerryType.SWEET_BERRY_BUSH, BerryType.BLUE_BERRY_BUSH);
-	
+	public static final EnumProperty<BerryType> BERRY_TYPE = EnumProperty.create("berry_type", BerryType.class);
 }
