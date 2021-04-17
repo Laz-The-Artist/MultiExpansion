eaedfc174d8dc9dfa6815f17509c370bcb489c6a
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-18 00:05:29 +0200

    ColoredSmokeParticle - complete

--- src/main/java/com/multiexpansion/client/particle/ColoredCampfireSmokeParticle.java
+++ src/main/java/com/multiexpansion/client/particle/ColoredCampfireSmokeParticle.java
@@ -19,23 +19,23 @@ public class ColoredCampfireSmokeParticle extends SpriteTexturedParticle {
 	private ColoredCampfireSmokeParticle(ClientWorld world, double xPos, double yPos, double zPos, double motionX, double motionY, double motionZ, boolean isSignal) {
 		
 		super(world, xPos, yPos, zPos);
-		this.multipleParticleScaleBy(3.0F);
+		this.setPower(3.0F);
 		this.setSize(0.25F, 0.25F);
 		
 		if (isSignal) {
 			
-			this.maxAge = this.rand.nextInt(50) + 280;
+			this.lifetime = this.random.nextInt(50) + 280;
 			
 		} else {
 			
-			this.maxAge = this.rand.nextInt(50) + 80;
+			this.lifetime = this.random.nextInt(50) + 80;
 			
 		}
 		
-		this.particleGravity = 3.0E-6F;
-		this.motionX = motionX;
-		this.motionY = motionY + (double)(this.rand.nextFloat() / 500.0F);
-		this.motionZ = motionZ;
+		this.gravity = 3.0E-6F;
+		this.xd = motionX;
+		this.yd = motionY + (double)(this.random.nextFloat() / 500.0F);
+		this.zd = motionZ;
 		
 	}
 	
@@ -46,27 +46,26 @@ public class ColoredCampfireSmokeParticle extends SpriteTexturedParticle {
 	}
 	
 	public void tick() {
+		this.xo = this.x;
+		this.yo = this.y;
+		this.zo = this.z;
 		
-		this.prevPosX = this.posX;
-		this.prevPosY = this.posY;
-		this.prevPosZ = this.posZ;
-		
-		if (this.age++ < this.maxAge && !(this.particleAlpha <= 0.0F)) {
+		if (this.age++ < this.lifetime && !(this.alpha <= 0.0F)) {
 			
-			this.motionX += (double)(this.rand.nextFloat() / 5000.0F * (float)(this.rand.nextBoolean() ? 1 : -1));
-			this.motionZ += (double)(this.rand.nextFloat() / 5000.0F * (float)(this.rand.nextBoolean() ? 1 : -1));
-			this.motionY -= (double)this.particleGravity;
-			this.move(this.motionX, this.motionY, this.motionZ);
+			this.xd += (double)(this.random.nextFloat() / 5000.0F * (float)(this.random.nextBoolean() ? 1 : -1));
+			this.zd += (double)(this.random.nextFloat() / 5000.0F * (float)(this.random.nextBoolean() ? 1 : -1));
+			this.yd -= (double)this.gravity;
+			this.move(this.xd, this.yd, this.zd);
 			
-			if (this.age >= this.maxAge - 60 && this.particleAlpha > 0.01F) {
+			if (this.age >= this.lifetime - 60 && this.alpha > 0.01F) {
 				
-				this.particleAlpha -= 0.015F;
+				this.alpha -= 0.015F;
 				
 			}
 			
 		} else {
 			
-			this.setExpired();
+			this.remove();
 			
 		}
 		
@@ -83,12 +82,12 @@ public class ColoredCampfireSmokeParticle extends SpriteTexturedParticle {
 			
 		}
 		
-		public Particle makeParticle(ColoredCampfireSmokeParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+		public Particle createParticle(ColoredCampfireSmokeParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
 			
 			ColoredCampfireSmokeParticle campfireparticle = new ColoredCampfireSmokeParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, false);
 			
-			campfireparticle.setAlphaF(0.9F);
-			campfireparticle.selectSpriteRandomly(this.spriteSet);
+			campfireparticle.setAlpha(0.9F);
+			campfireparticle.setSpriteFromAge(spriteSet);
 			campfireparticle.setColor(typeIn.getRed(), typeIn.getGreen(), typeIn.getBlue());
 			
 			return campfireparticle;
@@ -108,12 +107,12 @@ public class ColoredCampfireSmokeParticle extends SpriteTexturedParticle {
 			
 		}
 		
-		public Particle makeParticle(ColoredCampfireSmokeParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+		public Particle createParticle(ColoredCampfireSmokeParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
 			
 			ColoredCampfireSmokeParticle campfireparticle = new ColoredCampfireSmokeParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, true);
 			
-			campfireparticle.setAlphaF(0.95F);
-			campfireparticle.selectSpriteRandomly(this.spriteSet);
+			campfireparticle.setAlpha(0.95F);
+			campfireparticle.setSpriteFromAge(this.spriteSet);
 			campfireparticle.setColor(typeIn.getRed(), typeIn.getGreen(), typeIn.getBlue());
 			
 			return campfireparticle;
