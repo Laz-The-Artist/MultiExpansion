b3af0986e0dd15823b85def66e2e2f1ad17eda3c
LazTheArtist <kingofcreepersyt@gmail.com>
2021-10-18 20:14:40 +0200

    glowstone (and now also glow ink sac) force feeding

--- src/main/java/multiteam/multiexpansion/main/EventHandler.java
+++ src/main/java/multiteam/multiexpansion/main/EventHandler.java
@@ -4,8 +4,18 @@ import multiteam.multicore_lib.MultiCoreLib;
 import multiteam.multiexpansion.MultiExpansion;
 import multiteam.multiexpansion.main.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.InstantenousMobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.item.ItemEvent;
@@ -19,52 +29,82 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 public class EventHandler {
 
     @SubscribeEvent
-    public static void playerRightClickEvent(PlayerInteractEvent.RightClickBlock event){
-        if(!event.getWorld().isClientSide() && event.getPlayer().getItemInHand(event.getHand()).getItem() instanceof AxeItem){
-            BlockPos clickedPos = event.getHitVec().getBlockPos();
-            Block clickedBlock = event.getWorld().getBlockState(clickedPos).getBlock();
+    public static void playerRightClickBlockEvent(PlayerInteractEvent.RightClickBlock event){
+
+        Level level = event.getWorld();
+        Player player = event.getPlayer();
+        InteractionHand hand = event.getHand();
+        ItemStack handStack = player.getItemInHand(hand);
+        BlockPos clickedPos = event.getHitVec().getBlockPos();
+        Block clickedBlock = level.getBlockState(clickedPos).getBlock();
+
+        //Bark stripping
+        if(!level.isClientSide() && handStack.getItem() instanceof AxeItem){
             if(clickedBlock == Blocks.ACACIA_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.ACACIA_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.ACACIA_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.BIRCH_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.BIRCH_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.BIRCH_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.DARK_OAK_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.DARK_OAK_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.DARK_OAK_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.JUNGLE_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.JUNGLE_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.JUNGLE_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.OAK_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.OAK_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.OAK_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.SPRUCE_LOG){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.SPRUCE_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.SPRUCE_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.CRIMSON_STEM){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.CRIMSON_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.CRIMSON_BARK.get()));
                 }
             }
             if(clickedBlock == Blocks.WARPED_STEM){
                 for (int i = 0; i < 4; i++){
-                    Block.popResource(event.getWorld(), clickedPos, new ItemStack(ModItems.WARPED_BARK.get()));
+                    Block.popResource(level, clickedPos, new ItemStack(ModItems.WARPED_BARK.get()));
                 }
             }
         }
 
     }
+
+
+    @SubscribeEvent
+    public static void playerRightClickEntityEvent(PlayerInteractEvent.EntityInteract event){
+        Level level = event.getWorld();
+        Player player = event.getPlayer();
+        InteractionHand hand = event.getHand();
+        ItemStack handStack = player.getItemInHand(hand);
+        Entity entityTarget = event.getTarget();
+
+        //Glow effect Force-Feeding (Glowstone Dust / Glow Ink Sac)
+        if(!level.isClientSide() && entityTarget instanceof LivingEntity){
+            LivingEntity livingEntity = (LivingEntity) entityTarget;
+            if(handStack.getItem() == Items.GLOWSTONE_DUST || handStack.getItem() == Items.GLOW_INK_SAC){
+                livingEntity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 1200, 1));
+                if(!player.isCreative()){
+                    handStack.shrink(1);
+                }
+            }
+        }
+    }
+
+
 
 }
